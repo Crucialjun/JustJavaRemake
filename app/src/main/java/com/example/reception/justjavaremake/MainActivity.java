6f35d7fc6dda1b461353d77a7a1e3c9eca594f25
Crucialjun <nich.otieno@gmail.com>
2018-08-14 19:29:01 +0300

    final cleanup

--- app/src/main/java/com/example/reception/justjavaremake/MainActivity.java
+++ app/src/main/java/com/example/reception/justjavaremake/MainActivity.java
@@ -24,7 +24,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        int price = quantity * 5;
+        String priceMessage = "Total: Ksh" + price;
+        priceMessage = priceMessage + "\nThank you!";
         displayMessage(priceMessage);
         
     }
